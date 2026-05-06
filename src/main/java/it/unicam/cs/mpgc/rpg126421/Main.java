73b7704e88ce61cb189adf868ab7e1e8648dcbc4
valeriovirgili12 <valerio.virgili@studenti.unicam.it>
2026-05-06 20:20:20 +0200

    chore: initial project setup

---
+++ src/main/java/it/unicam/cs/mpgc/rpg126421/Main.java
@@ -0,0 +1,7 @@
+package it.unicam.cs.mpgc.rpg126421;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("RPG Game - it.unicam.cs.mpgc.rpg126421");
+    }
+}
